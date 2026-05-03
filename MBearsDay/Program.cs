5faf82929e8b74e5d769862f02cad9b8f19b5a49
Manoj Bhatty <manoj.bhatty@gmail.com>
2026-05-04 03:22:42 +0530

    Change to a worker service instead

--- MBearsDay/Program.cs
+++ MBearsDay/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MBearsDay
+namespace MBearsDay
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
         }
     }
 }
